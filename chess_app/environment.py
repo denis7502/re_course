3454347ad6cbbcd035e4d36876527136ca9becb0
Максим <zaitsev808@mail.ru>
2022-11-08 01:59:55 +0300

    renamed engine_use.py -> environment.py; renamed names of methods according camel case; add get_board_fen(), player_move(), env_move() and set_move_on_board()

--- chess_app/environment.py
+++ chess_app/environment.py
@@ -2,7 +2,10 @@ from dataclasses import replace
 from stockfish import Stockfish
 import numpy as np
 
-class ChessInterface():
+import chess
+
+
+class ChessInterface:
     """
     https://pypi.org/project/stockfish/
     Default parametrs
@@ -21,35 +24,39 @@ class ChessInterface():
     "UCI_LimitStrength": "false",
     "UCI_Elo": 1350
     """
-    def __init__(self) -> None:
-        self.stockfish = Stockfish(path="chess_app\src\stockfish_15_x64_avx2.exe")
 
-    def _strip(self, x):
+    def __init__(self, engine_path:str, verbose=False) -> None:
+        self.stockfish = Stockfish(path=engine_path)
+        self.verbose = verbose
+
+    @staticmethod
+    def _strip(x):
         return x.strip()
 
-    def printCurrentPos(self, type='base'):
-        
+    def print_board(self, type='base'):
         print(self.stockfish.get_board_visual())
 
-
-    def getCurTable(self):
+    def get_board_matrix(self):
         board = self.stockfish.get_board_visual().replace('+', '').replace('-', '').replace('|', '')
         np_board = np.array([])
         for line in board.split('\n')[:-2]:
             if line != '' and 'a  b  c' not in line:
-                np_board = np.append(np_board, np.array(line[1:-3].split('  ')[:8]))        
-        np_board = np_board.reshape(8,8)
+                np_board = np.append(np_board, np.array(line[1:-3].split('  ')[:8]))
+        np_board = np_board.reshape(8, 8)
         np_board[np_board == ''] = 0
         strp = np.vectorize(self._strip)
         np_board = strp(np_board)
-        
+
         return np_board
+
+    def get_board_fen(self):
+        return self.stockfish.get_fen_position()
 
     def reset(self):
         clear_pos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
         self.stockfish.set_fen_position(clear_pos)
 
-    def step(self, pos1, pos2, view_from ='white'):
+    def step(self, pos1, pos2, view_from='white'):
         try:
             check = self.stockfish.get_what_is_on_square(pos2)
             eat = False if check == None else True
@@ -58,20 +65,33 @@ class ChessInterface():
         except ValueError:
             return -1
 
-    def getTopSteps(self, n=5):
+    def get_top_steps(self, n=5):
         steps = self.stockfish.get_top_moves(n)
         array = np.array([])
         for step in steps:
             array = np.append(array, np.array([step['Move'], step['Centipawn'], step['Mate']]), axis=0)
-        array = array.reshape(n,3)
-        array = array[array[:,1].argsort()][::-1]
+        array = array.reshape(n, 3)
+        array = array[array[:, 1].argsort()][::-1]
         return array
 
-#"""test
-def main():
-    chs2 = ChessInterface()
-    print(chs2.step('d2', 'd4'))
-    chs2.printCurrentPos()
+    def player_move(self, move: str):
+        if self.stockfish.is_move_correct(move):
+            self.set_move_on_board(move)
+        else:
+            raise ValueError(f'Invalid move: {move}')
 
-main()
-#"""
+    def env_move(self) -> str:
+        best_move = self.stockfish.get_best_move()
+        self.set_move_on_board(best_move)
+        return best_move
+
+    def set_move_on_board(self, move) -> None:
+        """
+        return:
+            1 - фигура съедена
+            0 - ход без поедания фигуры
+        """
+        self.stockfish.make_moves_from_current_position([move])
+        # if figure := self.stockfish.get_what_is_on_square(move[2:]):
+        #     if figure is not None and self.verbose is True:
+        #         print(f'-- {figure} was captured.') # todo: это фигня работает не так, как надо, исправлю потом
