e3bab11051a3da9a0a4ff5c588cea648ea967e34
Maxim Zaitsev <zaisetv808@mail.ru>
2022-11-27 00:19:34 +0300

    modified update_qtable() in QAgent()

--- re_algoritms/agents.py
+++ re_algoritms/agents.py
@@ -7,8 +7,10 @@ from abc import ABC, abstractmethod
 class AbstractAgent(ABC):
     def __init__(self):
         self.board = chess.Board()
+        self.current_state = None
 
     def set_fen(self, fen: str):
+        self.current_state = fen
         self.board.set_fen(fen)
 
     @abstractmethod
@@ -34,7 +36,21 @@ class QAgent(AbstractAgent):
 
     def get_legal_moves(self, state):
         self.board.set_fen(state)
-        return list(self.board.legal_moves)
+        return [str(move) for move in self.board.legal_moves]
+
+    def get_opponent_moves(self, player_move):
+        self.board.push_san(player_move)
+        opponent_moves = [str(move) for move in self.board.legal_moves]
+        self.board.set_fen(self.current_state)
+        return opponent_moves
+
+    def get_next_states(self, opponent_moves):
+        states = list()
+        for opp_move in opponent_moves:
+            self.board.push_san(opp_move)
+            states.append(self.board.board_fen())
+            self.board.set_fen(self.current_state)
+        return states
 
     def return_move(self, state, e):
         self.set_fen(state)
@@ -43,13 +59,17 @@ class QAgent(AbstractAgent):
             print(' - exploratory')
             legal_moves = self.get_legal_moves(state)
             move = random.choice(legal_moves)
-            return str(move)
+            return move
         else:
             return str(self.get_move_with_max_qvalue(state))
 
     def update_qtable(self, state):
         if state not in self.q_table:
+            moves_states = dict()
             legal_moves = self.get_legal_moves(state)
+            for move in legal_moves:
+                opponent_moves = self.get_opponent_moves(move)
+                
             base_costs = [0.6 for _ in range(len(legal_moves))]
             new_dict = dict(zip(legal_moves, base_costs))
             self.add_to_qtable(state, new_dict)
